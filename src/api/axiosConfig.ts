7cf651c4e94371f23ea9eab71f02777306efbb9f
DougO28 <douguiortegaq@gmail.com>
2025-11-15 16:31:40 -0600

    Actualización de Axios

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,9 +1,9 @@
 // src/api/axiosConfig.ts
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000';
+//const API_BASE_URL = import.meta.env.VITE_API_BASE || 'http://127.0.0.1:8000';
 
-//const API_BASE_URL = 'https://admin-plantas.onrender.com';
+const API_BASE_URL = 'https://admin-plantas.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: `${API_BASE_URL}/api`,
